0ade3ba26fac77ede3686857133e9339e60fccac
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-09-29 00:51:56 +0300

    Created form

--- T7 Operating Systems Lab2/Form1.Designer.cs
+++ T7 Operating Systems Lab2/Form1.Designer.cs
@@ -28,20 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b25 = new System.Windows.Forms.Button();
+            this.b10 = new System.Windows.Forms.Button();
+            this.b50 = new System.Windows.Forms.Button();
+            this.b100 = new System.Windows.Forms.Button();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b1.Location = new System.Drawing.Point(113, 127);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(70, 70);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b2.Location = new System.Drawing.Point(189, 127);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(70, 70);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b5.Location = new System.Drawing.Point(265, 127);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(70, 70);
+            this.b5.TabIndex = 2;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b25
+            // 
+            this.b25.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b25.Location = new System.Drawing.Point(189, 203);
+            this.b25.Name = "b25";
+            this.b25.Size = new System.Drawing.Size(70, 70);
+            this.b25.TabIndex = 3;
+            this.b25.Text = "25";
+            this.b25.UseVisualStyleBackColor = true;
+            // 
+            // b10
+            // 
+            this.b10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b10.Location = new System.Drawing.Point(113, 203);
+            this.b10.Name = "b10";
+            this.b10.Size = new System.Drawing.Size(70, 70);
+            this.b10.TabIndex = 3;
+            this.b10.Text = "10";
+            this.b10.UseVisualStyleBackColor = true;
+            // 
+            // b50
+            // 
+            this.b50.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b50.Location = new System.Drawing.Point(265, 203);
+            this.b50.Name = "b50";
+            this.b50.Size = new System.Drawing.Size(70, 70);
+            this.b50.TabIndex = 4;
+            this.b50.Text = "50";
+            this.b50.UseVisualStyleBackColor = true;
+            // 
+            // b100
+            // 
+            this.b100.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.b100.Location = new System.Drawing.Point(189, 279);
+            this.b100.Name = "b100";
+            this.b100.Size = new System.Drawing.Size(70, 70);
+            this.b100.TabIndex = 5;
+            this.b100.Text = "100";
+            this.b100.UseVisualStyleBackColor = true;
+            // 
+            // tbInput
+            // 
+            this.tbInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.Location = new System.Drawing.Point(43, 71);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(140, 29);
+            this.tbInput.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(39, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "You gave me";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(261, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "And I can give you";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbOutput.Location = new System.Drawing.Point(265, 71);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(140, 29);
+            this.tbOutput.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 422);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(455, 396);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.b100);
+            this.Controls.Add(this.b50);
+            this.Controls.Add(this.b10);
+            this.Controls.Add(this.b25);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b25;
+        private System.Windows.Forms.Button b10;
+        private System.Windows.Forms.Button b50;
+        private System.Windows.Forms.Button b100;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbOutput;
     }
 }
 
